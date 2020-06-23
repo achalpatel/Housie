4578802d555f2efd8e18166f7e48b5d78b526fac
achalpatel <achalpatel008@gmail.com>
2020-06-23 11:55:54 -0700

    Working on Login functionality

--- app/src/main/java/com/example/housie/LoginActivity.java
+++ app/src/main/java/com/example/housie/LoginActivity.java
@@ -1,14 +1,70 @@
 package com.example.housie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button btnLogin;
+    private EditText emailTextView;
+    private EditText passwordTextView;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+    private static final String TAG = "LoginActivity";
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        btnLogin = findViewById(R.id.btn_login);
+        emailTextView = findViewById(R.id.id_email);
+        passwordTextView = findViewById(R.id.id_password);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginButton();
+            }
+        });
+
+    }
+    protected void loginButton(){
+        String email = emailTextView.getText().toString();
+        String password = passwordTextView.getText().toString();
+        if(!email.isEmpty() && !password.isEmpty()){
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Log.d(TAG, "onComplete: Logged in successfully");
+                        Toast.makeText(LoginActivity.this, "Welcome User", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }else if(task.isCanceled()){
+                        Log.w(TAG, "onComplete: Incorrect credentials", task.getException());
+                        Toast.makeText(LoginActivity.this, "Incorrect Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }else{
+            Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/housie/MainActivity.java
+++ app/src/main/java/com/example/housie/MainActivity.java
@@ -7,23 +7,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class MainActivity extends AppCompatActivity {
     private Button login_btn;
     private Button signup_btn;
+    private Button signout_btn;
+    private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         login_btn = findViewById(R.id.btn_login);
         signup_btn = findViewById(R.id.btn_signup);
+        signout_btn = findViewById(R.id.btn_signout);
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        if(currentUser!=null){
+            signup_btn.setVisibility(View.GONE);
+            login_btn.setVisibility(View.GONE);
+            signout_btn.setVisibility(View.VISIBLE);
+        }else {
+            signout_btn.setVisibility(View.GONE);
+        }
 
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
+                getIntent();
             }
         });
 
@@ -32,6 +49,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        signout_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                getIntent();
             }
         });
     }
