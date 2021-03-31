1d91460433cca007a3a718a8d2acf0c54012d07c
achalpatel <achalpatel008@gmail.com>
2021-03-31 15:54:15 -0400

    Friend Requests Updated To Transactions

--- app/src/main/java/com/example/adapter/SearchPeopleAdapter.java
+++ app/src/main/java/com/example/adapter/SearchPeopleAdapter.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.housie.R;
@@ -23,6 +24,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
@@ -92,32 +95,42 @@ public class SearchPeopleAdapter extends RecyclerView.Adapter<SearchPeopleAdapte
             public void onClick(View v) {
                 requestButton.setEnabled(false);
                 currentUserFriends.addToFriendReqSent(userId);
-                mDatabase.child(currentUser.getUid()).setValue(currentUserFriends).addOnCompleteListener(new OnCompleteListener<Void>() {
+                mDatabase.child(currentUser.getUid()).runTransaction(new Transaction.Handler() {
+                    @NonNull
                     @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        Log.d(TAG, "onComplete: FriendreqSent");
+                    public Transaction.Result doTransaction(@NonNull MutableData currentData) {
+                        UserFriends tempUser = currentData.getValue(UserFriends.class);
+                        if(tempUser==null){
+                            return Transaction.success(currentData);
+                        }
+                        currentData.setValue(currentUserFriends);
+                        return Transaction.success(currentData);
                     }
-                });
 
-                mDatabase.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        UserFriends selectedUserFriends = snapshot.getValue(UserFriends.class);
-                        selectedUserFriends.addToFriendReqReceived(currentUser.getUid());
-                        mDatabase.child(userId).setValue(selectedUserFriends).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@Nullable DatabaseError error, boolean committed, @Nullable DataSnapshot currentData) {
+                        Log.d(TAG, "onComplete: FriendreqSent");
+                        mDatabase.child(userId).runTransaction(new Transaction.Handler() {
+                            @NonNull
                             @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                            requestButton.setText("Sent");
+                            public Transaction.Result doTransaction(@NonNull MutableData currentData) {
+                                UserFriends tempUser = currentData.getValue(UserFriends.class);
+                                if(tempUser==null){
+                                    return Transaction.success(currentData);
+                                }
+                                tempUser.addToFriendReqReceived(currentUser.getUid());
+                                currentData.setValue(tempUser);
+                                return Transaction.success(currentData);
+                            }
+
+                            @Override
+                            public void onComplete(@Nullable DatabaseError error, boolean committed, @Nullable DataSnapshot currentData) {
+                                Log.d(TAG, "onComplete: FriendReqRec");
+                                requestButton.setText("Sent");
                             }
                         });
                     }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        Log.e(TAG, "onCancelled: friendrequestrec Failed"+error);
-                    }
                 });
-
             }
         });
     }
@@ -126,6 +139,5 @@ public class SearchPeopleAdapter extends RecyclerView.Adapter<SearchPeopleAdapte
     public int getItemCount() {
         return userResult.size();
     }
-
 
 }
